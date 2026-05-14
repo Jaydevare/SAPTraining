9ba835fa8d2a563e51feb7a3bc2e65824557782f
Digvijay Deavre <digvijaydevare@gmail.com>
2026-05-14 10:42:56 +0000

    second commit

---
+++ resue/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("demo.fragment.resue.controller.View1", {
+        onInit() {
+        }
+    });
+});
